138504cc8af8e9c403faedb1badeb32e23b8e587
joao.costa@ccc.ufcg.edu.br <ulquiorra212>
2020-09-20 12:54:27 -0300

    configurando conexão com o banco

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const dbConfig = require('./config/db.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json());
+
+require('./application/routes/patient.routes')(app);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
